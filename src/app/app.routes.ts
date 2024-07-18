12c095e4d162d4741a8558bf30440503f819ad80
Victor Zuccolotto <victor.zuccolotto@ufu.br>
2024-07-18 20:51:05 -0300

    #11 Implementação da pagina meus-agendamentos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { MainPageComponent } from './core/pages/main-page/main-page.component';
 import { AgendamentoComponent } from './core/pages/agendamento/agendamento.component';
+import { MeusAgendamentosComponent } from './core/pages/meus-agendamentos/meus-agendamentos.component';
 
 export const routes: Routes = [
     {
@@ -10,5 +11,9 @@ export const routes: Routes = [
     {
         path:"agendamento",
         component: AgendamentoComponent
+    },
+    {
+        path:"meus-agendamentos",
+        component: MeusAgendamentosComponent
     }
 ];

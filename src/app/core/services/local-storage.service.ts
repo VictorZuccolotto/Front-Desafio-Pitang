607346310f92d9d22720f6edafc180140d7538b4
Victor Zuccolotto <victor.zuccolotto@ufu.br>
2024-07-18 17:55:07 -0300

    9 Implementação do serviço para behaviorsubject e armazenamento em localstorage

--- src/app/core/services/local-storage.service.ts
+++ src/app/core/services/local-storage.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { PacienteInfo } from '../shared/types/paciente';
+import { Agendamento } from '../shared/types/agendamento';
 
 @Injectable({
   providedIn: 'root'
@@ -19,4 +20,17 @@ export class LocalStorageService {
   setPacienteInfo(paciente: PacienteInfo){
     localStorage.setItem('pacienteInfo', JSON.stringify(paciente))
   }
+
+  setAgendamentos(agendamentos: Agendamento[]){
+    localStorage.setItem('agendamentos', JSON.stringify(agendamentos))
+  }
+
+  getAgendamentos(): Agendamento[] | null{
+    const item = localStorage.getItem('agendamentos')
+    if(item){
+        return JSON.parse(item) as Agendamento[];
+    }
+    return null;
+  }
+
 }
